2e51adc1698bb9fee507e24e874620e978e900d0
Anusha K <anusha.krishnamoorthy00@gmail.com>
2023-02-01 12:53:37 -0500

    Patient Health Database setup

--- database_1.py
+++ database_1.py
@@ -8,6 +8,8 @@ def main_driver():
     db.append(create_patient_entry('Bob Boyles', 2, 24))
     db.append(create_patient_entry('Charles Lyles', 3, 65))
     print(db)
+    print(f'{db[0][0]}'s age: {db[0][1]}')
     
 if __name__ =="__main__":
     main_driver()
+    
